8acde09c2acd45ef3cc0f83b048ee4241754a2a6
Jai Gupta <jaiwork04@gmail.com>
2024-07-16 22:10:15 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,31 +1,20 @@
 const express = require('express');
-const cors = require('cors');
+const cors    = require('cors');
 
 const app = express();
 
 const Data = require('./data');
 
-const corsOptions = {
-  origin: 'https://readme-gen-ai.vercel.app',
-};
+// const corsOptions = {
+//   origin: <backend url>,
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/api/test', async (req, res) => {
   res.send('Hello World!');
 })
-
-
-app.get('/api/generate', async (req, res) => {
-
-  const { githubId, repoName, branchName, fileName } = req.query;
-
-  const resp = await Data.getReadme(githubId, repoName, branchName, fileName);
-
-  res.send(resp);
-
-});
 
 
 module.exports = app;
